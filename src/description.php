7d60fe8944265899dd93410cf71124fb9c036c2d
stellabakac <b00159322@mytudublin.ie>
2025-02-10 16:00:42 +0000

    Added description of the cat

--- src/description.php
+++ src/description.php
@@ -7,6 +7,13 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1>Max</h1>
+    <p>Cat breed: Domestic Shorthair</p>
+    <p>Color: Tuxedo</p>
+    <p>Gender: Male</p>
+    <p>Age: 3 years old</p>
+    <p>Weight: 10 lbs</p>
+    <p>Microchipped</p>
+    <p>Max is really friendly, gentle and intelligent cat. He gets along great with every animal. He really loves people, especially kids! He likes to gets a lot of pets, likes to nap a lot and follow the people around the house. Max is looking for forever home, where he can be fully loved and getting cared of! <3</p>
 </body>
-</html>
+</html> 
