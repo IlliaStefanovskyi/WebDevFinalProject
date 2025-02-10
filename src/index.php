7b46050b83c3c0b76045f1a2ccae5f5082039b06
Illia <B00165280@mytudublin.ie>
2025-02-10 16:14:55 +0000

    Edited the main page and made it to show the cats and their names using PHP loop and array

--- src/index.php
+++ src/index.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>searchbar</title>
+    <title>Homepage</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <?php
+    $catDesc = array("Bob","Larry","Win","Labs","DooDoo");
+    for($i = 0; $i < count($catDesc); $i++){
+        echo'<img src = "images/img1.jpg" height = 100>
+            <p>', $catDesc[$i], '</p>';
+    }
+    ?>
 
-    
 </body>
 </html>
