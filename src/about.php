4155696dec050002fdee66cf7f51d644229ed061
Illia <B00165280@mytudublin.ie>
2025-02-21 20:50:35 +0000

    About page done

--- src/about.php
+++ src/about.php
@@ -12,7 +12,61 @@
 </head>
 <body>
     <?php require 'ComponentsCode/search.php'; ?>
-    
+    <div class = "ourHistoryContainer">
+        <div class = "ourHistory">
+            <img src = "images/ourHistoryAboutImage.jpg" alt = "Image relevant to our history">
+            <div class = "ourHistoryLeftContainer">
+                <h1>Our history</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut rutrum, urna ut 
+                    pretium auctor, elit dolor vehicula orci, vitae scelerisque justo tortor 
+                    vel orci. Cras varius velit consectetur bibendum ultrices. Vestibulum ante 
+                    ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; 
+                    Aliquam nisi sapien, varius nec ligula eget, malesuada volutpat sapien. 
+                    Aliquam interdum ex ac mi fringilla, nec placerat justo sollicitudin. Nulla 
+                    facilisi. Praesent vitae urna vel elit imperdiet accumsan. Phasellus varius 
+                    urna finibus, bibendum ipsum vitae, porta ligula. Vestibulum aliquet odio 
+                    quis mi pretium, eget pharetra sapien accumsan. Maecenas orci nisi, eleifend 
+                    sed erat non, euismod scelerisque nisi. Vestibulum elit orci, egestas ut 
+                    posuere sit amet, ornare a enim. Maecenas eu volutpat sapien, at condimentum 
+                    lacus. Nulla et odio id mi ultricies iaculis vel ut eros. Phasellus vulputate 
+                    turpis in neque semper posuere.</p>
+            </div>
+        </div>
+    </div>
+    <div class = "ourServicesHeader">
+        <h1>Our services</h1>
+    </div>
+    <div class = "ourServicesContainer">
+        <div class = "ourServices">
+            <div class = "left">
+                <h2>Adoption</h2>
+                <p>Curabitur ac ex at purus condimentum venenatis vitae quis nunc. Proin venenatis 
+                    sem ut dolor bibendum bibendum. Aenean sed fermentum dui. Fusce porta molestie 
+                    metus, sed hendrerit urna tristique at. Vivamus consectetur dui non erat euismod 
+                    placerat. In consequat diam id lacus porttitor placerat. Suspendisse maximus 
+                    ipsum nec finibus porta. Praesent sodales ligula vitae tellus tincidunt, sed 
+                    lacinia ipsum fringilla. Aenean egestas venenatis dolor vitae iaculis. Lorem 
+                    ipsum dolor sit amet, consectetur adipiscing elit. Nam justo sapien, posuere 
+                    in ante sit amet, dignissim bibendum dolor. Sed fringilla accumsan lorem, nec 
+                    commodo diam. Maecenas finibus sit amet massa vel pretium. Morbi sem lectus, 
+                    sagittis nec gravida id, elementum eget magna. Cras nibh mi, sodales vitae sem et, 
+                    accumsan ultrices metus.</p>
+            </div>
+            <div class = "right">
+                <h2>Rescue</h2>
+                <p>Donec libero est, tempus non metus vitae, pellentesque rutrum ipsum. Suspendisse 
+                    potenti. Donec nec euismod leo, in maximus risus. Aliquam commodo ullamcorper 
+                    hendrerit. Nunc malesuada, lorem id lacinia lacinia, mauris neque mattis lacus, 
+                    nec facilisis turpis odio sed lectus. Ut commodo augue in mollis ornare. Curabitur 
+                    a tristique est, ut pulvinar lectus. Nulla vel dui mi. Quisque faucibus quam a orci 
+                    tempus, vitae mollis leo volutpat. Vivamus lacinia sollicitudin lorem, ac dapibus 
+                    felis. Phasellus orci eros, molestie vitae mi maximus, ullamcorper tincidunt erat. 
+                    Curabitur dignissim fermentum rhoncus. Integer at lectus eros. Nam elementum dui 
+                    mauris, vel fermentum elit pulvinar nec. Suspendisse nec libero enim. Pellentesque 
+                    sollicitudin ut lacus a iaculis.</p>
+            </div>
+        </div>
+    </div>
     <?php require 'ComponentsCode/footer.php'; ?>
 </body>
 </html>
