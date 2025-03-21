441e8711caaaae30480116deede56aa34662560f
Illia <B00165280@mytudublin.ie>
2025-03-21 20:04:14 +0000

    Edited classes to correspond to requirements

--- src/classes/Employee.php
+++ src/classes/Employee.php
@@ -2,10 +2,10 @@
 final class Employee extends User{
     public $employeeId;
     public $jobTitle;
-    public $managerId;
-    function __construct($employeeId, $name, $jobTitle, $phoneNum, $managerId, $email){
+    public $manager;#partial composition, just pass a manager object
+    function __construct($employeeId, $name, $jobTitle, $phoneNum, $manager, $email){
         parent::__construct($email,$name, $phoneNum);
-        $this->managerId = $managerId;
+        $this->manager = $manager;
         $this->employeeId = $employeeId;
         $this->jobTitle = $jobTitle;
     }
--- src/classes/User.php
+++ src/classes/User.php
@@ -1,12 +1,37 @@
 <?php 
+#generalisation
 abstract class User {
     #has no password parameter, since it's a security threat
-    public $email;
-    public $name;
-    public $phoneNum;
+    private $email;
+    private $name;
+    private $phoneNum;
     function __construct($email, $name, $phoneNum){
         $this->email = $email;
         $this->name = $name;
+        $this->phoneNum = $phoneNum;
+    }
+    #encapsulation
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getPhoneNum() {
+        return $this->phoneNum;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setPhoneNum($phoneNum) {
         $this->phoneNum = $phoneNum;
     }
 }
